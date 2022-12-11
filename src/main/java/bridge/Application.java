47887cac32fb6611455bb3622944d780f6279b46
HubCreator <chocomilk8604@gmail.com>
2022-12-11 17:20:41 +0900

    refactor(Application) : run을 static으로

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,13 +1,8 @@
 package bridge;
 
-import bridge.domain.BridgeMaker;
-import bridge.manager.GameManager;
-
 public class Application {
 
     public static void main(String[] args) {
-        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
-        GameManager gameManager = new GameManager(bridgeMaker);
-        gameManager.run();
+        GameManager.run();
     }
 }
